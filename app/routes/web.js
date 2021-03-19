6b8cb68296b77863b1d078afc933159a8ce8b275
LuisFreitas97 <lgf1997@outlook.pt>
2021-03-19 12:21:21 +0000

    rota para fazer upload de ficheiro excel feita.

--- app/routes/web.js
+++ app/routes/web.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { ExcelToJson } from '../services/Excel/ExcelToJson.js'
 import asyncHandler  from 'express-async-handler';
+import { uploadFile } from '../services/Excel/UploadExcel.js'
+
 
 export const router = express.Router();
 
@@ -12,3 +14,8 @@ router.post("/insertExcelData", asyncHandler(async (req, res, next) => {
   var result = await ExcelToJson.convertExcelToJson(req);
   res.json(result);
 }));
+
+// router.post("/upload", upload.single("file"), excelController.upload);
+router.post("/upload", uploadFile.single("file"), function (req, res) {
+  res.json(req.file.filename);
+});
