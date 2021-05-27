75841f789f45f098be9cef7bb2b11ee4043a6943
LuisFreitas97 <lgf1997@outlook.pt>
2021-05-27 22:01:43 +0100

    guardar os dados metereológicos na bd com a subsecção.

--- app/services/WeatherAPI/WeatherAPI.js
+++ app/services/WeatherAPI/WeatherAPI.js
@@ -12,47 +12,62 @@ export class WeatherAPI {
         var center = {};
         var apiBody = {};
         var headers = { "auth": { "username": process.env.WEATHER_USER, "password": process.env.WEATHER_PASS }, "content-type": "application/x-www-form-urlencoded" };
+        var error = false;
 
         await Ajax.getRequest(process.env.DB_MICROSERVICE + '/drawAreas', {
             params: {
                 keepOrder: true
-            }}, function(data) {
-                var drawAreas = data.data;
-
-                if (drawAreas && drawAreas.length) {
-                    drawAreas.forEach(function (value) {
-                        //calculate centroid point 
-                        center = polygonCenter(value.geometry);
-                        center.BGRI11 = value.properties.BGRI11;
-                        centers.push(center);
-                        // Get weather data from API
-                        apiBody.coord = center.coordinates[0] + "," + center.coordinates[1];
-                        apiBody.vars = req.body.vars;
-                        Ajax.postRequest(process.env.WEATHER_API, qs.stringify(apiBody), headers, function (data) {
-                            data = JsonHelper.convertJson(data);
-                            result = WeatherAPI.insertWeatherInDb(data,"weatherAPI");
-                        }, function (error) {
-                            // result = { 'msg': error.response.data, 'code': error.response.status };
-                        });
+            }
+        }, async function (data) {
+            var drawAreas = data.data;
+            
+            if (drawAreas && drawAreas.length) {
+                // drawAreas.forEach(function (value) {
+                    for(const value of drawAreas ){
+                    //calculate centroid point 
+                    center = polygonCenter(value.geometry);
+                    center.BGRI11 = value.properties.BGRI11;
+                    centers.push(center);
+                    // Get weather data from API
+                    apiBody.coord = center.coordinates[0] + "," + center.coordinates[1];
+                    apiBody.vars = req.body.vars;
+                    await Ajax.postRequest(process.env.WEATHER_API, qs.stringify(apiBody), headers, function (data) {
+                        data.BGRI11 = center.BGRI11;
+                        data = JsonHelper.convertJson(data);
+                        WeatherAPI.insertWeatherInDb(data, "weatherAPI");
+                    }, function (error) {
+                        // result = { 'msg': error.response.data, 'code': error.response.status };
                     });
-                }
-                result = { "msg": "success", "data": req.body, "code": 201 };
-            }, function(error) {
-                result = { 'msg': error.response.data, 'code': error.response.status };
-            });
-        return result;
-    }
-    static async insertWeatherInDb(data, collectionName) {
-        var result;
-        var db_ms_url = process.env.DB_MICROSERVICE + '/insertJsonData';
-        var body = { "data": data, "collectionName": collectionName };
-
-        await Ajax.postRequest(db_ms_url, body, {}, function (data) {
-            result = { 'msg': data, 'code': 201 };
+                };
+            }
         }, function (error) {
-            result = { 'msg': error.response.data, 'code': error.response.status };
+            error = true;
         });
+        if(error){
+            result = { 'msg': "internal error", 'code': 500 };
+        }else{
+            result = { "msg": "success", "data": "success", "code": 201 };
+        }
+        console.log("aqui");
         return result;
     }
+
+    // await Promise.all(files.map(async (file) => {
+    //     const contents = await fs.readFile(file, 'utf8')
+    //     console.log(contents)
+    // }));
+      
+    static async insertWeatherInDb(data, collectionName) {
+    var result;
+    var db_ms_url = process.env.DB_MICROSERVICE + '/insertJsonData';
+    var body = { "data": data, "collectionName": collectionName };
+
+    await Ajax.postRequest(db_ms_url, body, {}, function (data) {
+        result = { 'msg': data, 'code': 201 };
+    }, function (error) {
+        result = { 'msg': error.response.data, 'code': error.response.status };
+    });
+    return result;
+}
 };
 
